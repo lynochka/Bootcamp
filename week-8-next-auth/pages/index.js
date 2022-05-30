3b54d6b4dd6f6204b178cb69903056e1a9e02305
lynochka <lina.astrakova@gmail.com>
2022-05-30 08:25:14 +0200

    Fix tailwind className

--- week-8-next-auth/pages/index.js
+++ week-8-next-auth/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
     return (
       <svg
         role="status"
-        class="m-3 w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        className="m-3 w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
