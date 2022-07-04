8ae2241a6ebf4a28af299d005b1c9e41358d8a8d
lynochka <lina.astrakova@gmail.com>
2022-07-04 19:32:46 +0200

    Remove test log

--- typescript-getting-started/multimath/js/app.js
+++ typescript-getting-started/multimath/js/app.js
@@ -2,7 +2,6 @@
 function startGame() {
     var messagesElement = document.getElementById("messages");
     messagesElement.innerText = "Welcome to MultiMath! Starting new game...";
-    console.log("Hi");
 }
 document.getElementById("startGame").addEventListener("click", startGame);
 //# sourceMappingURL=app.js.map
