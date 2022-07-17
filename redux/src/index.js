c890bfa8019e4290fbb12e079f7ba691e79e5982
lynochka <lina.astrakova@gmail.com>
2022-07-17 17:17:23 +0200

    Define redux store

--- redux/src/index.js
+++ redux/src/index.js
@@ -4,10 +4,17 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
+import configureAppStore from "./redux/configureAppStore";
+import { Provider as ReduxProvider } from "react-redux";
 
+// can be useful to pass initial state into the store
+// if server rendering or initalizing your Redux store with data from local storage
+const store = configureAppStore();
 render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ReduxProvider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ReduxProvider>,
   document.getElementById("app")
 );
---
+++ redux/src/redux/configureAppStore.js
@@ -0,0 +1,18 @@
+//TODO: pass slice reducers directly as in https://redux-toolkit.js.org/api/configureStore
+
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import rootReducer from "./reducers";
+
+export default function configureAppStore(preloadedState) {
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+    devTools: process.env.NODE_ENV !== "production",
+    preloadedState,
+  });
+  return store;
+}
+
+// redux-immutable-state-invariant: is added to the store by default by configureStore and getDefaultMiddleware
+// devTools: true should automatically enable support for the Redux DevTools browser extension.
