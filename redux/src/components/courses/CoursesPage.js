6dee645cf7b04770c08d155c2200b57bcfa3a778
lynochka <lina.astrakova@gmail.com>
2022-07-17 15:03:10 +0200

    Examples class binding

--- redux/src/components/courses/CoursesPage.js
+++ redux/src/components/courses/CoursesPage.js
@@ -1,8 +1,34 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
+  // less code than constructor(props) + super(props) + this.state = {...}
+  state = {
+    course: {
+      title: "",
+    },
+  };
+
+  //arrow function inherits the binding context of their enclosing scope
+  // better than this.handleChange = this.handleChange.bind(this); in the constructor
+  handleChange = (event) => {
+    // unless arrow function or binded: this in ...this.state refers to the change caller
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course }); // object shorthand syntax
+  };
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form>
+        <h2>Courses</h2>
+        <h3>Add course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
   }
 }
 
