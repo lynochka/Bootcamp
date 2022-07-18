464bbf700fa09a820b61541cc84bcaefc6b7e6bc
lynochka <lina.astrakova@gmail.com>
2022-07-18 18:49:42 +0200

    Bootstrap v5 format course form

--- redux/src/components/courses/ManageCoursePage.js
+++ redux/src/components/courses/ManageCoursePage.js
@@ -1,10 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { loadCourses } from "../../redux/actions/courseActons";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
 
-function ManageCoursePage({ courses, authors, loadCourses, loadAuthors }) {
+function ManageCoursePage({
+  courses,
+  authors,
+  loadCourses,
+  loadAuthors,
+  ...props
+}) {
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState({});
+
+  setCourse;
+  setErrors;
+
   useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch((error) => {
@@ -18,15 +32,12 @@ function ManageCoursePage({ courses, authors, loadCourses, loadAuthors }) {
     }
   }, []);
 
-  return (
-    <>
-      <h2>Manage Course</h2>
-    </>
-  );
+  return <CourseForm course={course} errors={errors} authors={authors} />;
 }
 
 // expect dispatch to be passed in if we omit mapDispatchToProps
 ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
   courses: PropTypes.array.isRequired,
   authors: PropTypes.array.isRequired,
   loadCourses: PropTypes.func.isRequired,
@@ -36,6 +47,7 @@ ManageCoursePage.propTypes = {
 // removed ownProps as the second argument
 function mapStateToProps(state) {
   return {
+    course: newCourse,
     courses: state.courses,
     authors: state.authors,
   };
