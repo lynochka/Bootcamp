8a2238e21cb144e95a11d19764e20bda6102d56f
lynochka <lina.astrakova@gmail.com>
2022-07-18 18:06:51 +0200

    Add course routing

--- redux/src/components/App.js
+++ redux/src/components/App.js
@@ -5,6 +5,7 @@ import AboutPage from "./about/AboutPage";
 import Header from "./common/Header";
 import PageNotFound from "./PageNotFound";
 import CoursesPage from "./courses/CoursesPage";
+import ManageCoursePage from "./courses/ManageCoursePage";
 
 //exact is needed for '/' to only match HomePage
 function App() {
@@ -15,6 +16,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/courses" element={<CoursesPage />} />
+        <Route path="/course/:slug" element={<ManageCoursePage />} />
+        <Route path="/course" element={<ManageCoursePage />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
