ef31a8987e1e61556a138d2f60ab7635bc9cb47a
lynochka <lina.astrakova@gmail.com>
2022-07-18 18:07:11 +0200

    ManageCoursePage as function Component

--- redux/src/components/courses/ManageCoursePage.js
+++ redux/src/components/courses/ManageCoursePage.js
@@ -1,13 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { loadCourses } from "../../redux/actions/courseActons";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 
-class ManageCoursePage extends React.Component {
-  componentDidMount() {
-    const { courses, authors, loadCourses, loadAuthors } = this.props;
-
+function ManageCoursePage({ courses, authors, loadCourses, loadAuthors }) {
+  useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch((error) => {
         alert("Loading courses failed" + error);
@@ -18,15 +16,13 @@ class ManageCoursePage extends React.Component {
         alert("Loading authors failed" + error);
       });
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <>
-        <h2>Manage Course</h2>
-      </>
-    );
-  }
+  return (
+    <>
+      <h2>Manage Course</h2>
+    </>
+  );
 }
 
 // expect dispatch to be passed in if we omit mapDispatchToProps
