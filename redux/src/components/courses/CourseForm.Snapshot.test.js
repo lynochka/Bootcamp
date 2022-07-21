0f048226148e469e10d3d09d95f97837c9f8d41e
lynochka <lina.astrakova@gmail.com>
2022-07-21 14:56:18 +0200

    Example jest setup

---
+++ redux/src/components/courses/CourseForm.Snapshot.test.js
@@ -0,0 +1,6 @@
+import React from "react";
+import CourseForm from "./CourseForm";
+import render from "react-test-renderer";
+import { courses, authors } from "../../../tools/mockData";
+
+it("sets submit button label to 'Saving' when saving is true", () => {});
